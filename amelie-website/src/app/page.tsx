568f54beffad92ecee67c4d9f17dbe137f02b863
amelieasberger <amelie.asberger@gmx.de>
2025-01-20 11:42:41 +0100

    created basic next js app

---
+++ amelie-website/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <Header />
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
---
+++ amelie-website/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function Header(): React.JSX.Element {
+
+    return (
+        <div>
+            Helloo
+        </div>
+    );
+}
